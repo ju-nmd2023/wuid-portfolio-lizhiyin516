b28ab0fbcc07bad81bf898faddbdc30e2d2949d8
Zhiyin Li <lizh21nv@student.ju.se>
2023-12-05 16:26:21 +0100

    add js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-        var btn  = document.getElementById("actAnima");
-        btn.addEventListener("click",function(){
-            alert('Thanks for question!');
-            var anima = document.getElementById("anima");
-            anima.classList.remove("animation");
+        var btn  = document.getElementById("actAnima");//get the elements from html
+        btn.addEventListener("click",function(){//add an event listener
+            alert('Thanks for question!');//take place of onclick
+            var anima = document.getElementById("anima");//get another element which let the pic move
+            anima.classList.remove("animation");//delet the former one and add the second time for pic action
             anima.offsetHeight = anima.offsetHeight; // force reflow
-            anima.classList.add("animation");
+            anima.classList.add("animation");//add the animation 
         });
  
     
