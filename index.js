d7f0ce0fdfa55832da0bf05ff0e66c2ecdd9df69
Zhiyin Li <lizh21nv@student.ju.se>
2023-12-06 14:31:31 +0100

    var deleted

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
-        var btn  = document.getElementById("actAnima");//get the elements from html
+        const btn  = document.getElementById("actAnima");//get the elements from html
         btn.addEventListener("click",function(){//add an event listener
             alert('Thanks for question!');//take place of onclick
-            var anima = document.getElementById("anima");//get another element which let the pic move
+            const anima = document.getElementById("anima");//get another element which let the pic move
             anima.classList.remove("animation");//delet the former one and add the second time for pic action
             anima.offsetHeight = anima.offsetHeight; // force reflow
             anima.classList.add("animation");//add the animation 
 
-            var communication = document.getElementById("communication");
+            const communication = document.getElementById("communication");
             communication.value="";//Dingxie helped with this
         });
  
